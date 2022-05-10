eb6e319564661cdb1a813cc5e66e49ceda637f89
Surendar S <96053487+Radnerus2021@users.noreply.github.com>
2022-05-10 09:48:43 +0530

    Delete basics directory

--- basics/dowhile.sh
+++
@@ -1,9 +0,0 @@
-read -p "enter the start value:" a
-read -p "enter the boundary:" b
-until [ $a -gt $b ]
-
-do
-  echo $a
-  a=`expr $a + 1`
-
-done
--- basics/switchcase.sh
+++
@@ -1,9 +0,0 @@
-read -p "enter your Fav car:" cars
-#cars=$(tr '[:lower:]' '[:upper:]' <<<$cars)
-shopt -s nocasematch
-case $cars in
-   "bmw") echo "I said better one";;
-   "audi") echo "I said better";;
-   "TATA") echo "Perfect";;
-    *) echo "Good";;
-esac
