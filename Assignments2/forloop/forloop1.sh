83b53695e9fa3dc9b28cf4c46f0e25d6e67e53aa
Surendar S <96053487+Radnerus2021@users.noreply.github.com>
2022-05-14 10:18:29 +0530

    Delete Assignments2 directory

--- Assignments2/forloop/forloop1.sh
+++
@@ -1,8 +0,0 @@
-read -p "Enter the number:" n
-
-
-for (( i=0 ;i<$n+1; i++ ))
-do
-	echo "$i in power of 2^$i is: $((2**$i))"
-done
-
--- Assignments2/whileloop/while1.sh
+++
@@ -1,9 +0,0 @@
-read -p "Enter the number:" n
-
-
-
-while [ $n -le 8 ]
-do
-	echo "$((2**n))"
-	((n++))
-done
